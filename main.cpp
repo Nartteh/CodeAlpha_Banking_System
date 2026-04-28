ef6f1367179285bf66e90e5495604b4b2ed29432
Nartteh <lnocansey@st.knust.edu.gh>
2026-04-28 17:59:56 +0100

    Add switch case for menu options

--- main.cpp
+++ main.cpp
@@ -25,6 +25,34 @@ int main(){
 
     cin >> menu;
 
+    switch(menu){
+        case 1:
+            cout << "Feature coming soon!" << endl;
+            break;
+        case 2:
+            cout << "Feature coming soon!" << endl;
+            break;
+        case 3:
+            cout << "Feature coming soon!" << endl;
+            break;
+        case 4:
+            cout << "Feature coming soon!" << endl;
+            break;
+        case 5:
+            cout << "Feature coming soon!" << endl;
+            break;
+        case 6:
+            cout << "Feature coming soon!" << endl;
+            break;
+        case 7:
+            cout << "Thank you for using CodeAlpha Banking System. Goodbye!" << endl;
+            break;
+        default:
+            cout << "Invalid option. Please try again." << endl;
+    }
+
+
+
 } while (menu !=7);
 
         return 0;
