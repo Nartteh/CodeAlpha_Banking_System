2d73e93d85dd273b7866daf711f38e8d911c5387
Nartteh <lnocansey@st.knust.edu.gh>
2026-05-18 01:45:32 +0100

    Implement Check Balance feature

--- main.cpp
+++ main.cpp
@@ -139,9 +139,48 @@ void withdrawMoney(){
         cout << "Insufficient funds! Minimum balance of GHS 30 must remain." << endl;
         return;
     }
-    
+
     accounts[index].balance -= amount;
     cout << "Withdrawal successful! New balance : GHS " << accounts[index].balance << endl;
+}
+
+void checkBalance() {
+    int accNumber;
+
+    cout << "Enter your Account Number :" << endl;
+    cin >> accNumber;
+
+    int index = -1;
+    for (int i = 0; i < accounts.size(); i++)
+    {
+        if (accounts[i].accountNumber == accNumber)
+        {
+            index = i;
+            break;
+        }
+    }
+
+    if (index == -1)
+    {
+        cout << "Account not found!" << endl;
+        return;
+    }
+
+    string enteredPin;
+    cout << "Enter your PIN : " << endl;
+    cin >> enteredPin;
+
+    if (enteredPin != accounts[index].pin)
+    {
+        cout << "Incorrect PIN!" << endl;
+        return;
+    }
+
+
+    cout << "Account Holder: " << accounts[index].name << endl;
+    cout << "Account Number: " << accounts[index].accountNumber << endl;
+    cout << "Your current balance is : GHS " << accounts[index].balance << endl;
+
 }
 
 int main(){
@@ -179,7 +218,7 @@ int main(){
             withdrawMoney();
             break;
         case 4:
-            cout << "Feature coming soon!" << endl;
+            checkBalance();
             break;
         case 5:
             cout << "Feature coming soon!" << endl;
